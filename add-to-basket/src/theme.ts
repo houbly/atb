2981382e05b1215db3905edf20cf944f378f0f58
Houston Blyden <houston.bylden@hollandandbarrett.com>
2022-02-25 18:22:08 +0000

    add theme

---
+++ add-to-basket/src/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+// A custom theme for this app
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    }
+  },
+});
+
+export default theme;
