20452bc0e1d248a5dd499a5d40a4b4abf18ef3dd
Houston Blyden <houston.bylden@hollandandbarrett.com>
2022-03-03 19:19:00 +0000

    make header prettier

--- add-to-basket/src/components/Header/index.tsx
+++ add-to-basket/src/components/Header/index.tsx
@@ -1,28 +1,31 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import StoreIcon from '@mui/icons-material/RocketLaunchOutlined';
 
 export default function ButtonAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color="transparent">
+    <Box sx={{ flexGrow: 1 }} mb={4}>
+      <AppBar position="static" color="primary" elevation={0}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+          <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Store
-          </Typography>
+            <StoreIcon fontSize="large" />
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, textTransform: 'uppercase', fontWeight: 700 }}
+            >
+              Houston's shop
+            </Typography>
+          </Stack>
         </Toolbar>
       </AppBar>
     </Box>
